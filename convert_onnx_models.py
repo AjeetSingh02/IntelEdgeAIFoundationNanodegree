3f8c26f43ea70e8e30698b55b929c4367e45c64e
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2020-01-08 22:50:12 +0530

    Create convert_onnx_models.py

---
+++ convert_onnx_models.py
@@ -0,0 +1,6 @@
+# To convert
+
+! python /opt/intel/openvino/deployment_tools/model_optimizer/mo.py --input_model model.onnx
+
+# DOCS
+# https://docs.openvinotoolkit.org/latest/_docs_MO_DG_prepare_model_convert_model_Convert_Model_From_ONNX.html
