cb5d90b61ced2dda72ee5972c5d6c2026ef9a254
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2020-01-12 09:36:33 +0530

    Delete chapter3_convert_onnx_models.py

--- chapter3_convert_onnx_models.py
+++
@@ -1,6 +0,0 @@
-# To convert ONNX Model
-
-! python /opt/intel/openvino/deployment_tools/model_optimizer/mo.py --input_model model.onnx
-
-# DOCS
-# https://docs.openvinotoolkit.org/latest/_docs_MO_DG_prepare_model_convert_model_Convert_Model_From_ONNX.html
