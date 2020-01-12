376628aeaabecd54f6ec768e1567f82309359bbb
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2020-01-12 10:29:17 +0530

    Update and rename handling_input_streams.py to handling_input_streams_APP.py

--- Chapter5/handling_input_streams_APP.py
+++ Chapter5/handling_input_streams_APP.py
@@ -19,18 +19,36 @@ def get_args():
 
 def capture_stream(args):
     ### TODO: Handle image, video or webcam
+    capture = cv2.VideoCapture(input_stream)
+    
+    capture.open(args.input)
+
+    while capture.isOpened():
+        flag, frame = cap.read()
+        if not flag:
+            break
+            
+    key_pressed = cv2.waitKey(60)
+    
+    if key_pressed == 27:
+        break
 
     ### TODO: Get and open video capture
 
     ### TODO: Re-size the frame to 100x100
+    image = cv2.resize(frame, (100, 100))
 
     ### TODO: Add Canny Edge Detection to the frame, 
     ###       with min & max values of 100 and 200
     ###       Make sure to use np.dstack after to make a 3-channel image
+    edges = cv2.Canny(image,100,200)
 
     ### TODO: Write out the frame, depending on image or video
+    cv2.imwrite('output.jpg', edges)
 
     ### TODO: Close the stream and any windows at the end of the application
+    capture.release()
+    cv2.destroyAllWindows()
 
 
 def main():
@@ -40,3 +58,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    
+    
+    
+    
+
+# To test:
+# python app.py -i blue-car.jpg
+# python app.py -i test_video.mp4
